4e31de17d026d5bd8990dfde1a0d822931cc8a7f
Alain Benbassat <alain@businessandcode.eu>
2022-12-08 22:01:01 +0100

    json

--- src/MuntpuntEvents.php
+++ src/MuntpuntEvents.php
@@ -20,7 +20,7 @@ class MuntpuntEvents {
       ->addWhere('extra_evenement_info.activiteit_status', '=', self::eventStatusCommunicatieOK)
       ->addWhere('is_active', '=', TRUE)
       ->addOrderBy('start_date', 'ASC')
-      ->setLimit(500)
+      ->setLimit(1)
       ->execute();
     foreach ($events as $event) {
       $eventArray[] = self::convertEventToObject($event);
@@ -32,10 +32,53 @@ class MuntpuntEvents {
   static private function convertEventToObject($event) {
     $e = new \stdClass();
 
-    foreach (['id', 'title', 'summary', 'start_date', 'end_date'] as $field) {
+    foreach (['id', 'title', 'start_date', 'end_date', 'summary', 'description'] as $field) {
       $e->$field = $event[$field];
     }
 
+    $e->registration_url = \CRM_Utils_System::baseURL() . 'civicrm/event/register?reset=1&id=' . $event['id'];
+    $e->registration_label = $event['registration_link_text'];
+    $e->location = self::getEventLocation($event['id']);
+
+    $e->related_events  = self::getRelatedEvents($event['id']);
+var_dump($event);exit;
     return $e;
+  }
+
+  static private function getEventLocation($eventId) {
+
+  }
+
+  static private function getRelatedEvents($eventId) {
+    $relatedEvents = [];
+
+    $parentEventId = \CRM_Core_DAO::singleValueQuery("select parent_id from civicrm_recurring_entity where entity_table = 'civicrm_event' and entity_id = $eventId");
+    if ($parentEventId) {
+      $sql = "
+        select
+          re.entity_id
+        from
+          civicrm_recurring_entity re
+        inner join
+          civicrm_event e on e.id = re.entity_id and re.entity_table = 'civicrm_event'
+        where
+          re.entity_table = 'civicrm_event'
+        and
+          re.parent_id = $parentEventId
+        and
+          re.entity_id <> $eventId
+        and
+          e.start_date >= now()
+        and
+          e.is_active = 1
+      ";
+
+      $dao = \CRM_Core_DAO::executeQuery($sql);
+      while ($dao->fetch()) {
+        $relatedEvents[] = $dao->entity_id;
+      }
+    }
+
+    return $relatedEvents;
   }
 }
